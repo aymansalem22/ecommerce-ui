6136add230304f3286d11a041e22ecee394b0363
aymansalem22 <s.aymansalem@gmail.com>
2023-03-15 23:26:56 +0100

    Building the UI for Product API: Adding a Product

--- src/router/index.js
+++ src/router/index.js
@@ -3,22 +3,14 @@ import Home from '../views/HomeView.vue'
 import AddCategory from "../views/Category/AddCategory";
 import Category from "../views/Category/Category";
 import EditCategory from "../views/Category/EditCategory";
-
+import AddProduct from "../views/Product/AddProduct";
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
   },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
+ 
   {
     path: "/admin/category/add",
     name: "AddCategory",
@@ -32,7 +24,12 @@ const routes = [
   {
     path: "/admin/category/:id",
     name: "EditCategory",
-    component: EditCategory
+    component: EditCategory,
+  },
+  {
+    path: "/admin/product/add",
+    name: "AddProduct",
+    component: AddProduct,
   }
 ];
 
